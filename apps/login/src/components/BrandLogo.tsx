4525a6303f234c8840bc2778be12dec0c2bd2118
Fernando Lopez <flopez@evercommerce.com>
2025-04-22 12:35:42 -0700

    adding dynamic logo based on cookie.

--- apps/login/src/components/BrandLogo.tsx
+++ apps/login/src/components/BrandLogo.tsx
@@ -1,17 +1,30 @@
 'use client'
 
 import Image from "next/image";
+import {useEffect, useState} from "react";
 
 export default function BrandLogo() {
-  return (
-      <div className='flex basis-[75%] justify-center bg-background-light-100 shadow-xl'>
-          <Image
-              src='/logo/cl-logo.svg'
-              alt="EverPro Logo"
-              width={314}
-              height={259}
-              sizes="100vw"
-          />
-      </div>
-  )
+    const [logo, setLogo] = useState<string>('/logo/everpro-logo.svg');
+    
+    useEffect(() => {
+        const application = document.cookie.split('; ').find(cookie => cookie.trim().startsWith('application'))?.split('=')[1];
+        if (application === 'customer-lobby') {
+            setLogo('/logo/cl-logo.svg');
+        } else if (application === 'pulsem') {
+            setLogo('/logo/pm-logo.svg');
+        } else {
+            setLogo('/logo/everpro-logo.svg');
+        }
+    }, []);
+    return (
+        <div className='flex basis-[75%] justify-center bg-background-light-100 shadow-xl'>
+            <Image
+                src={logo}
+                alt="EverPro Logo"
+                width={314}
+                height={259}
+                sizes="100vw"
+            />
+        </div>
+    )
 }
